2a1d4e24bc487c179d77edc27b4fd0f98143b0bb
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-06 23:03:00 +0530

    Component for header added

---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+import React from "react"
+
+function Header() {
+
+    return (
+        // <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom">
+        //     <a class="navbar-brand" href="#">Navbar</a>
+        //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //         <span class="navbar-toggler-icon"></span>
+        //     </button>
+
+        //     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        //         <ul class="navbar-nav mr-auto">
+        //         <li class="nav-item active">
+        //             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        //         </li>
+        //         <li class="nav-item">
+        //             <a class="nav-link" href="#">List Country</a>
+        //         </li>
+        //         <li class="nav-item dropdown">
+        //             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        //             Dropdown
+        //             </a>
+        //             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        //             <a class="dropdown-item" href="#">Action</a>
+        //             <a class="dropdown-item" href="#">Another action</a>
+        //             <div class="dropdown-divider"></div>
+        //             <a class="dropdown-item" href="#">Something else here</a>
+        //             </div>
+        //         </li>
+        //         <li class="nav-item">
+        //             <a class="nav-link disabled" href="#">Disabled</a>
+        //         </li>
+        //         </ul>
+        //         <form class="form-inline my-2 my-lg-0">
+        //         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+        //         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        //         </form>
+        //     </div>
+        // </nav>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top justify-content-between navbar-custom">
+            {/* <a className="navbar-brand" href="#">
+                <img className="Logo" src="./brand-logo.png"/>
+            </a> */}
+            <button className="navbar-toggler" type="button" data-toggle="collapse" 
+                    data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+            </button>
+            
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav mr-auto">
+                    <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-item nav-link" href="/countries">List Country</a>
+                    {/* <a className="nav-item nav-link" href="#">Pricing</a>
+                    <a className="nav-item nav-link disabled" href="#">Disabled</a> */}
+                </div>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search by Brand" aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+
+        </nav>
+    );
+}
+
+
+export default Header;
