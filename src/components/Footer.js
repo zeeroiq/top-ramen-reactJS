fa657a67046f31315c74c1bcef183802bcd150a1
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-07 17:14:35 +0530

    Footer updated. close #18

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+
+    return <div className="bg-dark text-center text-white-50">
+        <p className="p-2">Copyright © I 😎 {new Date().getFullYear()}</p>
+    </div>
+}
+
+
+export default Footer;
