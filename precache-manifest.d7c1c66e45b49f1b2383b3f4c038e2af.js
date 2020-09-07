dae0c55cd5005b7077c9953d1d4d59e5a920f6f4
zeero1q <shrikantvishwakrma74@gmail.com>
2020-09-07 15:26:00 +0530

    Updates

---
+++ precache-manifest.d7c1c66e45b49f1b2383b3f4c038e2af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029b2d534a2e4a2dd4a0da3719ba41ef",
+    "url": "/top-ramen-reactJS/index.html"
+  },
+  {
+    "revision": "1a9a1fe448673790d2ef",
+    "url": "/top-ramen-reactJS/static/css/main.a5a3bda3.chunk.css"
+  },
+  {
+    "revision": "c8705e623a84d267341e",
+    "url": "/top-ramen-reactJS/static/js/2.30d44da9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/top-ramen-reactJS/static/js/2.30d44da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9a1fe448673790d2ef",
+    "url": "/top-ramen-reactJS/static/js/main.00ccee62.chunk.js"
+  },
+  {
+    "revision": "b5005c3781a62d9174b2",
+    "url": "/top-ramen-reactJS/static/js/runtime-main.102dd0dc.js"
+  },
+  {
+    "revision": "1635151b6701c06a4211d5d640b646de",
+    "url": "/top-ramen-reactJS/static/media/brand-logo.1635151b.png"
+  }
+]);
